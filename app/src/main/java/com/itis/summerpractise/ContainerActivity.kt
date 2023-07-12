4653897e21b22eba0b198d3a018e14ab8713554b
Arslan Nikolaev <nikolaevarslan1@yandex.ru>
2023-07-12 21:47:53 +0300

    проверка

--- app/src/main/java/com/itis/summerpractise/ContainerActivity.kt
+++ app/src/main/java/com/itis/summerpractise/ContainerActivity.kt
@@ -2,6 +2,7 @@ package com.itis.summerpractise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -10,16 +11,17 @@ class ContainerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_container)
-        findViewById<BottomNavigationView>(R.id.bottomNav).apply{
-            setupWithNavController((supportFragmentManager.findFragmentById(R.id.container) as? NavHostFragment)?.navController!!)
-        /*
-            setOnItemSelectedListener {
-                when(it.itemId){
-                    R.id.itemi -> navigateUpTo()
-                    }
-                }
-            }*/
+
+        val controller =
+            (supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment)
+                .navController
+
+        findViewById<BottomNavigationView>(R.id.bottomNav).apply {
+            setupWithNavController(controller)
         }
     }
+}
 
+enum class Month(var order: Int) {
+    Jan(0), Feb(6), Apr(2), May(-1)
 }
--- app/src/main/java/com/itis/summerpractise/SettingFragment.kt
+++ app/src/main/java/com/itis/summerpractise/SettingFragment.kt
@@ -1,8 +1,14 @@
 package com.itis.summerpractise
 
 import android.os.Bundle
+import android.text.style.TtsSpan.ARG_TEXT
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.itis.summerpractise.ProfileFragment.Companion.ARG_COUNTRY
+import com.itis.summerpractise.ProfileFragment.Companion.ARG_ICON
+import com.itis.summerpractise.ProfileFragment.Companion.ARG_ID
+import com.itis.summerpractise.ProfileFragment.Companion.ARG_NAME
 import com.itis.summerpractise.databinding.FragmentSettingsBinding
 
 class SettingFragment: Fragment(R.layout.fragment_settings) {
@@ -10,10 +16,28 @@ class SettingFragment: Fragment(R.layout.fragment_settings) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSettingsBinding.bind(view)
+
+        val id = arguments?.getInt(ARG_ID, -1)
+        val text = arguments?.getString(ARG_ICON, "")
+        val name = arguments?.getString(ARG_NAME, "")
+        val country = arguments?.getString(ARG_COUNTRY, "")
+        // Используйте извлеченные данные для обновления иконки и текста
+        binding?.textView?.text = id.toString()
+        binding?.text?.text = name.toString()
+        binding?.text2?.text = country.toString()
+        val imageView = binding?.image
+        if (imageView != null) {
+            Glide.with(this)
+                .load(text)
+                .into(imageView)
+        }
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
     }
+
+
 }
